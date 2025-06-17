982658d287524b44fa34547e2b099943aff3d292
anjs1998 <anjs1998@gmail.com>
2025-06-17 18:03:50 +0900

    unify

--- zipinfo-project/frontend/src/api/axiosApi.js
+++ zipinfo-project/frontend/src/api/axiosApi.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-export const axiosApi = axios.create({
-  //baseURL: "http://localhost:8080",
-  baseURL: "https://cmh-boardproject.store",
+export const axiosAPI = axios.create({
+  baseURL: "http://localhost:8080",
   withCredentials: true,
-  // headers : {"Content-Type" : "application/json"}
-  // withCredentials : true // 쿠키 포함 설정
-  // 서버에서도 클라이언트가 보낸 쿠키를 받아줄 준비 해야함!
+  headers: { "Content-Type": "application/json" }, // axios는 자동 변환해주기 때문에 명시 안해도 괜찮다
+  // withCredentials : true 쿠키 포함 설정
+  // 서버에서도 클라이언트가 보낸 쿠키를 받아줄 준비 필요
   // credential 허용 설정 필요함
-  // -> JWT 사용 시 중요한 옵션!
+  // -> JWT 사용 시 중요한 옵션
 });
