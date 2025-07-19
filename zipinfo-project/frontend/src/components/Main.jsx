87e76fbab8c40a1dd341f6d1a3945b1d58a07271
EC2 Default User <ec2-user@ip-172-31-39-228.ap-northeast-2.compute.internal>
2025-07-19 11:53:20 +0000

    지피티개새

--- zipinfo-project/frontend/src/components/Main.jsx
+++ zipinfo-project/frontend/src/components/Main.jsx
@@ -578,9 +578,7 @@ const Main = () => {
 
       {mainAd && mainAd.adImgUrl !== null ? (
         <div className="banner">
-<img
-  src={`${import.meta.env.VITE_API_BASE_URL}${mainAd.adImgUrl}`}     alt="배너광고 이미지"
-   />        </div>
+<img src={mainAd.adImgUrl}                          alt="배너광고 이미지" />     </div>
       ) : (
         <div />
       )}
