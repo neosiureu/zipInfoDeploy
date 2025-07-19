bead9b9173a9eaffa1af2741b7991b2aeba19e39
EC2 Default User <ec2-user@ip-172-31-39-228.ap-northeast-2.compute.internal>
2025-07-19 08:35:33 +0000

    썅

--- zipinfo-project/frontend/src/components/Main.jsx
+++ zipinfo-project/frontend/src/components/Main.jsx
@@ -578,7 +578,7 @@ const Main = () => {
       {mainAd && mainAd.adImgUrl !== null ? (
         <div className="banner">
           <img
-   src={`${import.meta.env.VITE_API_BASE_URL}/images/advertiseImg/${
+   src={`${import.meta.env.VITE_API_BASE_URL}${
      mainAd.adImgUrl.replace(/\.png$/, ".jpg")
    }`}
    alt="배너광고 이미지"
