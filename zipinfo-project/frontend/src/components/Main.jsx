10697fac78e0bbb0b2144b695c62fde673becaa2
EC2 Default User <ec2-user@ip-172-31-39-228.ap-northeast-2.compute.internal>
2025-07-19 09:09:58 +0000

    qqqqqq

--- zipinfo-project/frontend/src/components/Main.jsx
+++ zipinfo-project/frontend/src/components/Main.jsx
@@ -25,7 +25,7 @@ const Main = () => {
   const [saleList, setSaleList] = useState([]);
   const [mainAd, setMainAd] = useState(null);
  useEffect(() => {
-    console.log("DEBUG banner src: " + import.meta.env.VITE_API_BASE_URL + resp.data.adImgUrl);
+    console.log("[DEBUG] mainAd.adImgUrl ▶", mainAd?.adImgUrl);
   }, [mainAd]);
   const [searchContent, setSearchContent] = useState("");
 
@@ -578,10 +578,7 @@ const Main = () => {
 
       {mainAd && mainAd.adImgUrl !== null ? (
         <div className="banner">
-          <img
-  src={`${import.meta.env.VITE_API_BASE_URL}${mainAd.adImgUrl.replace(/\.png$/, ".jpg")}`}
-  alt="배너광고 이미지"
-/>
+      src={`${import.meta.env.VITE_API_BASE_URL}${mainAd.adImgUrl}`}
         </div>
       ) : (
         <div />
